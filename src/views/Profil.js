6cd7590c34db0aa967586f0598ee3019c2b5563b
toniwidodo123 <litoninot12345@gmail.com>
2021-10-23 07:19:35 -0700

    upload file

--- src/views/Profil.js
+++ src/views/Profil.js
@@ -1,9 +1,138 @@
 import React from "react";
-import {ScrollView,Text,View,TouchableOpacity} from "react-native";
+import {ScrollView,Text,View,TouchableOpacity,Image} from "react-native";
 import { Header,Input,BottomSheet,ListItem,Avatar,Card} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker'; // Migration from 2.x.x to 3.x.x => showImagePicker API is removed.
+import axios from "axios";
 
 export default class Profil extends React.Component { 
+	constructor(props){
+		super(props);
+
+		this.state = {
+			resourcePath : {
+			}
+		}
+
+		this.selectFile = this.selectFile.bind(this);
+		this.uploadToServer = this.uploadToServer.bind(this);
+	}
+
+	selectFile(){
+		var options = {
+		  title: 'Select Image',
+		  customButtons: [
+		    { 
+		      name: 'customOptionKey', 
+		      title: 'Choose file from Custom Option' 
+		    },
+
+		  ],
+		  storageOptions: {
+		    skipBackup: true,
+		    path: 'images',
+		  },
+		};
+
+		launchImageLibrary(options, res => {
+		  console.log('Response = ', res);
+		  if (res.didCancel) {
+		    console.log('User cancelled image picker');
+		  } else if (res.error) {
+		    console.log('ImagePicker Error: ', res.error);
+		  } else if (res.customButton) {
+		    console.log('User tapped custom button: ', res.customButton);
+		    // alert(res.customButton);   
+		  } else {
+		    let source = res;
+		    this.setState({
+		      resourcePath: source.assets[0],
+		    },() => {
+		    	this.uploadToServer();
+		    });
+		  }
+		});
+
+			// launchCamera(options, res => {
+		//     console.log('Response = ', res);
+		//     if (res.didCancel) {
+		//       console.log('User cancelled image picker');
+		//     } else if (res.error) {
+		//       console.log('ImagePicker Error: ', res.error);
+		//     } else if (res.customButton) {
+		//       console.log('User tapped custom button: ', res.customButton);
+		//       // alert(res.customButton);   
+		//     } else {
+		//       let source = res;
+		//       this.setState({
+		//         resourcePath: source.assets[0],
+		//       });
+		//     }
+		//   });
+  	}
+
+  	uploadToServer(){    		
+		var data = new FormData();
+		data.append("photo", {
+			uri : this.state.resourcePath.uri,	
+			type : this.state.resourcePath.type,
+   			name: this.state.resourcePath.fileName,
+		});
+
+		data.append("hello","test");
+
+		// form.append('photo',this.state.resourcePath);
+
+		fetch('http://192.168.8.215:8000/api/test-upload', {
+         	method: "POST",
+         	headers: {
+            	'Content-Type': 'multipart/form-data',         
+            	'Accept': 'application/json',
+         	},
+         	body: data,         	         
+      	}).then((response) => {
+      		// console.log(response.status);
+      		return response.json()
+      	})
+   		.catch((error) => {
+   			console.log("Error" + error);
+      		// alert("ERROR " + error)
+   		})
+   		.then((responseData) => {
+   			console.log(responseData);
+      		// alert("Succes " + responseData)
+   		});
+
+		// console.log(this.state.resourcePath);
+		// var form = new FormData();
+		// form.append("photo", {
+		// 	uri : this.state.resourcePath.uri,	
+		// 	type : this.state.resourcePath.type,
+  //  			name: this.state.resourcePath.filename,
+		// });
+
+		// var config = {
+		//     method: 'post',
+		//     url: 'http://192.168.8.215:8000/api/test-upload',
+		//     headers: {
+		//        	'Content-Type': 'multipart/form-data',         
+		//         'Accept': 'application/json',
+		//     },
+		//     data:  JSON.stringify(form),
+		// };
+
+  //   	axios(config)
+  //       .then((response) => {
+  //           console.log(JSON.stringify(response.data));
+  //       })
+  //       .catch((error) => {
+  //           console.log(error);
+  //       })
+  //       .then((response) => {
+  //       	console.log(response);
+  //       })
+  	}
+
 	render(){
 		return (
 			<View>
@@ -25,6 +154,18 @@ export default class Profil extends React.Component {
 				
 				<ScrollView>
 					<Card>
+					  <Image
+			            source={{ uri: this.state.resourcePath.uri }}
+			            style={{ width: 200, height: 200 }}/>
+
+			          <Text style={{ alignItems: 'center' }}>
+			            {this.state.resourcePath.uri}
+			          </Text>
+
+			          <TouchableOpacity onPress={(event) => this.selectFile(event)}>
+			              <Text style={{color:'black'}}>Select File</Text>
+			          </TouchableOpacity>       
+
 						<View style={{alignItems : 'center'}}>
 							<Avatar  
 								rounded
